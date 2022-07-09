a2b44afe801ecd809dd3db5b5167c6213d429eb5
lecav <lecav30@outlook.com>
2022-07-09 12:49:21 -0500

    Resolved beginner problem

---
+++ 0-1000 - Beginner Level/16. Roller Coaster/info.md
@@ -0,0 +1,5 @@
+Difficulty: 285
+
+Attempts: 1
+
+Link: https://www.codechef.com/submit/MINHEIGHT
---
+++ 0-1000 - Beginner Level/16. Roller Coaster/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve() {
+    int x, h;
+    cin >> x >> h;
+    cout << (x >= h ? "YES" : "NO") << endl;
+}
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
