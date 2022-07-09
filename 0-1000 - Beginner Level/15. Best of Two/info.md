5a939e44393349b96e5973ed336259e21340b9a7
lecav <lecav30@outlook.com>
2022-07-09 12:43:15 -0500

    Resolved beginner problem

---
+++ 0-1000 - Beginner Level/15. Best of Two/info.md
@@ -0,0 +1,5 @@
+Difficulty: 284
+
+Attempts: 1
+
+Link: https://www.codechef.com/submit/BESTOFTWO
---
+++ 0-1000 - Beginner Level/15. Best of Two/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    cout << max(x, y) << endl;
+}
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
