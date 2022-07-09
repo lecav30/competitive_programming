f131ee1ab1c643a7a0ef14f4a34d39ea9cf78924
lecav <lecav30@outlook.com>
2022-07-09 00:56:55 -0500

    Resolved beginner problem

---
+++ 0-1000 - Beginner Level/10. Burgers/info.md
@@ -0,0 +1,5 @@
+Difficulty: 263
+
+Attempts: 1
+
+Link: https://www.codechef.com/submit/BURGERS
---
+++ 0-1000 - Beginner Level/10. Burgers/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << min(a, b) << endl;
+}
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
