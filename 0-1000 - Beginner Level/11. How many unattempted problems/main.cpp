0946cc0c54f50b8445a35129822761df77b5a7be
lecav <lecav30@outlook.com>
2022-07-09 01:07:22 -0500

    Resolved beginner problem

---
+++ 0-1000 - Beginner Level/11. How many unattempted problems/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    cout << x - y << endl;
+}
+
+int main() {
+    solve();
+    return 0;
+}
