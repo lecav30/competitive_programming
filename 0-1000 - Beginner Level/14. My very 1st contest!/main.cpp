a1ad24a70252af4eeabbde1af60c9289f46b158d
lecav <lecav30@outlook.com>
2022-07-09 12:29:02 -0500

    Resolved beginner problem

---
+++ 0-1000 - Beginner Level/14. My very 1st contest!/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << (n - a) << " " << (n - a - b) << endl;
+}
+
+int main() {
+    // int tc;
+    // cin >> tc;
+    // while (tc--) {
+        solve();
+    // }
+    return 0;
+}
