690596029c4392cb8d5bffc232ac9e3cadee8432
lecav <lecav30@outlook.com>
2022-07-09 13:00:02 -0500

    Resolved beginner problem

---
+++ 0-1000 - Beginner Level/18. Total Prize Money/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    cout << 10 * x + 90 * y << endl;
+}
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
