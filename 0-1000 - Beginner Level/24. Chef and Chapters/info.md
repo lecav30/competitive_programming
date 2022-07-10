61c8b8e4fba4f2890d2325880010885adf1276ac
lecav <lecav30@outlook.com>
2022-07-10 09:43:06 -0500

    Resolved problem

---
+++ 0-1000 - Beginner Level/24. Chef and Chapters/info.md
@@ -0,0 +1,5 @@
+Difficulty: 350
+
+Attempts: 1
+
+Link: https://www.codechef.com/submit/START36
---
+++ 0-1000 - Beginner Level/24. Chef and Chapters/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve()
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << x * y * z << endl;
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
