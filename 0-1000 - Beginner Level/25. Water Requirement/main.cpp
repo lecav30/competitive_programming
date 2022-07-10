945a2cb81ae75ba217e6e5d8195e45c0a1002ebc
lecav <lecav30@outlook.com>
2022-07-10 09:48:58 -0500

    Template problem

---
+++ 0-1000 - Beginner Level/25. Water Requirement/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+void solve()
+{
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
