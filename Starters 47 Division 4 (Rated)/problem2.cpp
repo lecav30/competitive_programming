93a141523f9eecea7c8e8ee01b2a4c0ad81c17e4
lecav <lecav30@outlook.com>
2022-07-13 12:36:22 -0500

    Solved problem

---
+++ Starters 47 Division 4 (Rated)/problem2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+typedef long long LL;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << (n * 60) / 20 << endl;
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
