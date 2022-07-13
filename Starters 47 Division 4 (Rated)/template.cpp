6acd4002943719107144a991dedbc1d50d0bf3d0
lecav <lecav30@outlook.com>
2022-07-13 12:35:41 -0500

    Template cpp

---
+++ Starters 47 Division 4 (Rated)/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+typedef long long LL;
+
+void solve()
+{
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
