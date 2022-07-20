c926af98f0cd5e744b3884df410a9ac17ce908e8
lecav <lecav30@outlook.com>
2022-07-20 12:36:40 -0500

    Template

---
+++ Starters 48 Division 4 (Rated)/template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+typedef long long LL;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
