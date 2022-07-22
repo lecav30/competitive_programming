2a91967d4f8674aa2531c15188d47da1b6038cd9
lecav <lecav30@outlook.com>
2022-07-22 16:44:25 -0500

    Solved problem with help - READ THE ANY IN THE STATEMENT

--- Starters 48 Division 4 (Rated)/pE.cpp
+++ Starters 48 Division 4 (Rated)/pE.cpp
@@ -9,55 +9,20 @@ void solve()
     cin >> n;
     string str;
     cin >> str;
-    string revStr = str;
-    reverse(revStr.begin(), revStr.end());
-    if (str == revStr)
-        cout << str << endl;
+
+    int ones = 0, zeroes = 0;
+    for (int i = 0; i < n; ++i)
+        if (str[i] == '1')
+            ones++;
+        else
+            zeroes++;
+    if (zeroes >= ones)
+        for (int i = 0; i < zeroes; ++i)
+            cout << '0';
     else
-    {
-        for (int i = 0; i < str.length() / 2; ++i)
-        {
-            if (str[i] != str[str.length() - i - 1])
-            {
-                string temp = str, aux = str;
-
-                // borrar al inicio
-                temp.erase(temp.begin() + i);
-                string temp1 = temp;
-                // borrar al final
-                aux.erase(aux.begin() + (aux.length() - i - 1));
-                string temp2 = aux;
-                // cout << "temp2 " << temp2 << endl;
-
-                string revTemp1 = temp1;
-                reverse(revTemp1.begin(), revTemp1.end());
-                string revTemp2 = temp2;
-                reverse(revTemp2.begin(), revTemp2.end());
-                // borar al final el que se borro al inicio
-                temp.erase(temp.begin() + (temp.length() - i - 1));
-                string revTemp = temp;
-                reverse(revTemp.begin(), revTemp.end());
-
-                if (revTemp1 == temp1)
-                    str = temp1;
-                else if (revTemp2 == temp2)
-                    str = temp2;
-                else if (revTemp == temp)
-                    str = temp;
-                else
-                    str = temp1;
-
-                // cout << "temp " << temp << endl;
-                // cout << "revTemp " << revTemp << endl;
-            }
-            revStr = str;
-            reverse(revStr.begin(), revStr.end());
-            // terminar cuando sea palindromo
-            if (str == revStr)
-                break;
-        }
-        cout << str << endl;
-    }
+        for (int i = 0; i < ones; ++i)
+            cout << '1';
+    cout << endl;
 }
 
 int main()
