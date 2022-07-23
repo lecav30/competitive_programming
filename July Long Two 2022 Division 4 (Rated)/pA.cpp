b9dbf5bcf5a6b5d25cfe375ab87afa8088a35c2f
lecav <lecav30@outlook.com>
2022-07-23 14:14:34 -0500

    Solved problem

---
+++ July Long Two 2022 Division 4 (Rated)/pA.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+
+void solve()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << min(x, y) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
