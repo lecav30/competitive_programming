35698ee38f48c187428687f2e9825191fef937e3
lecav <lecav30@outlook.com>
2022-07-24 23:46:09 -0500

    Time Limit Exceeded

--- July Long Two 2022 Division 4 (Rated)/pF.cpp
+++ July Long Two 2022 Division 4 (Rated)/pF.cpp
@@ -3,16 +3,31 @@ using namespace std;
 #define endl "\n"
 #define ll long long
 
-int gcd(int a, int b) {
-   if (b == 0)
-   return a;
-   return gcd(b, a % b);
-}
-
 void solve()
 {
     int n, m;
     cin >> n >> m;
+    int a = n, b = n;
+    int maximized = 0;
+    for (int i = n; i < m; ++i)
+    {
+        if (maximized >= m - i)
+            break;
+        for (int j = m; j > i; --j)
+        {
+            if ((i % n == 0 && j % n == 0) || j % i == 0)
+            {
+                if (abs(i - j) > maximized)
+                {
+                    maximized = abs(i - j);
+                    a = i;
+                    b = j;
+                    break;
+                }
+            }
+        }
+    }
+    cout << a << " " << b << endl;
 }
 
 int main()
