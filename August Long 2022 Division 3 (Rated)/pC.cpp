79552a7cef90c11414643432eb4127b8d6ddb8e3
lecav30 <lecav30@outlook.com>
2022-08-14 19:55:29 -0500

    Wrong Answer

--- August Long 2022 Division 3 (Rated)/pC.cpp
+++ August Long 2022 Division 3 (Rated)/pC.cpp
@@ -31,86 +31,78 @@ vector<int> arrToVec(int arr[], int n) {
     return vec;
 }
 
-/*
- * # BITMASK
-    O(2^n * n) -> n <= 22
-    (1 << n) = (1 * 2^n)
-    Subsets
-    for (int mask = 0; mask < (1 << n); ++mask)
-    {
-        cout << "mask " << mask << " -> ";
-        for (int bit = 0; bit < n; ++bit)
-        {
+void subsets(vector<int> &v) {
+    int n = v.size();
+    for (int mask = 0; mask < (1 << n); ++mask) {
+        for (int bit = 0; bit < n; ++bit) {
             // subconjunto
-            if ((mask >> bit) & 1)
-                cout << " " << C[bit];
+            if ((mask >> bit) & 1) cout << " " << v[bit];
             // complemento del subconjunto
-            if ((~mask >> bit) & 1)
-                cout << " " << C[bit];
+            if ((~mask >> bit) & 1) cout << " " << v[bit];
         }
         cout << endl;
     }
- */
+}
 
 void solve() {
     int n;
     cin >> n;
-    int arr[n - 1];
-    for (int i = 0; i < n - 1; ++i) cin >> arr[i];
+    n--;
+    int arr[n];
+    for (int i = 0; i < n; ++i) cin >> arr[i];
 
-    ll tt = arr[0];
-    int a = 0, b = -1;
+    ll tt = 0;
+    int a = -1, b = -1;
     int progressB = 0, progressA = 0;
 
-    while (b < n) {
-        // bool prog = false;
-        // if (a - b > 1) prog = true;
-
-        if (arr[a] >= arr[a + 1]) {
-            progressB += arr[a + 1];
-            a++;
-        } else {
-            if (a - b) {
-                // waiting time
-                tt += arr[a + 1] - progressA;
-                progressA = 0;
-                a++;
-            } else {
-                int temp = arr[b + 1] - progressB;
-                if (a == n - 1) a--;
-                if (arr[a + 1] >= temp) {
-                    progressB += temp;
-                    progressA += temp;
-                } else {
-                    progressB += arr[a + 1];
+    while (b < n - 1) {
+        int temp;
+        if (a == n - 1)
+            temp = arr[a];
+        else
+            temp = arr[a + 1];
+        if (a - b >= 1) {
+            if (arr[b + 1] - progressB >= temp) {
+                progressB += temp;
+                if (a < n - 1) {
                     a++;
+                    progressA = 0;
                 }
+            } else {
+                progressA += arr[b + 1] - progressB;
+                progressB += arr[b + 1] - progressB;
+            }
+        } else {
+            if (a == n - 1)
+                progressB += temp;
+            else {
+                tt += temp - progressA;
+                progressA += temp - progressA;
             }
         }
         if (progressB == arr[b + 1]) {
             tt += progressB;
-            progressB = 0;
             b++;
+            progressB = 0;
+        }
+        if (progressA == arr[a + 1]) {
+            a++;
+            progressA = 0;
         }
     }
     cout << tt << endl;
 }
 
 int main() {
-    // clock_t ini = clock();
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int tc;
     cin >> tc;
     while (tc--) solve();
-    /* for (int i = 0; i < tc; ++i) */
-    /* { */
-    /*     cout << "Case " << i + 1 << ": "; */
-    /*     solve(); */
-    /*     cout << endl; */
-    /* } */
-    // clock_t fin = clock();
-    // double run_time = (double)(fin - ini) / CLOCKS_PER_SEC;
-    // cout << "runtime: " << fixed << setprecision(2) << run_time << endl;
+    // for (int i = 0; i < tc; ++i) {
+    //     cout << "Case " << i + 1 << ": ";
+    //     solve();
+    //     cout << endl;
+    // }
     return 0;
 }
