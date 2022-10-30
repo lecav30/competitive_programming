fafbcfba831697bf41cadf619dc617b748be8671
lecav30 <lecav30@outlook.com>
2022-10-29 20:06:25 -0500

    Not solved

---
+++ 1400-1500 - Beginner Level/1. Average Permutation/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+
+    // a b c d e f
+}
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) solve();
+    return 0;
+}
