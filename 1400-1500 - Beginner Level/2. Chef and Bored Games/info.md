4b477101bbb05a49ab7ade06cc12ff86b0013a01
lecav30 <lecav30@outlook.com>
2022-11-05 01:28:10 -0500

    Not solved

---
+++ 1400-1500 - Beginner Level/2. Chef and Bored Games/info.md
@@ -0,0 +1,5 @@
+Difficulty: 1402
+
+Attempts:
+
+Link: https://www.codechef.com/submit/PCJ18B
---
+++ 1400-1500 - Beginner Level/2. Chef and Bored Games/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    ll ans = n;
+    for (int i = 3; i <= n; ++i) {
+        // ans += 9 
+        /*
+           *****
+           *****
+           *****
+           *****
+           *****
+           */
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--) solve();
+    return 0;
+}
