5f9b41d2b1b4fc5941a29f1454522eea41f4ead9
lecav30 <lecav30@outlook.com>
2022-12-31 19:27:19 -0500

    All is weird

--- codechef/1000-1200 - Beginner Level/5. Chef and Dolls/info.md
+++ codechef/1000-1200 - Beginner Level/5. Chef and Dolls/info.md
@@ -1,6 +1,6 @@
 Difficulty: 1012
 
-Attempts: 1
+Attempts: 3
 
 Link: https://www.codechef.com/problems/MISSP
 
--- codechef/1000-1200 - Beginner Level/5. Chef and Dolls/main.cpp
+++ codechef/1000-1200 - Beginner Level/5. Chef and Dolls/main.cpp
@@ -1,10 +1,21 @@
 #include <algorithm>
+#include <cstring>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
 void solve() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i += 2)
+        if (arr[i] != arr[i + 1]) {
+            cout << arr[i] << "\n";
+            break;
+        }
 }
 
 int main() {
