944873514cefd2662007491c38cb6dc24542910d
lecav30 <lecav30@outlook.com>
2023-01-01 15:07:31 -0500

    So easy

--- codechef/1000-1200 - Beginner Level/6. How much Scholarship/info.md
+++ codechef/1000-1200 - Beginner Level/6. How much Scholarship/info.md
@@ -1,5 +1,5 @@
 Difficulty: 1012
 
-Attempts: 0
+Attempts: 1
 
 Link: https://www.codechef.com/problems/ZCOSCH
--- codechef/1000-1200 - Beginner Level/6. How much Scholarship/main.cpp
+++ codechef/1000-1200 - Beginner Level/6. How much Scholarship/main.cpp
@@ -6,13 +6,19 @@
 using namespace std;
 
 void solve() {
+    int n;
+    cin >> n;
+    if (n <= 50) cout << "100\n";
+    else if (n <= 100) cout << "50\n";
+    else cout << "0\n";
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int tc;
-    cin >> tc;
-    while (tc--) solve();
+    /* int tc; */
+    /* cin >> tc; */
+    /* while (tc--) solve(); */
+    solve();
     return 0;
 }
