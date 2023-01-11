ed0fafbfb799fbce7affd16256a8a91844849e85
lecav30 <lecav30@outlook.com>
2023-01-11 18:07:10 -0500

    Check the variables

--- codechef/1000-1200 - Beginner Level/9. Chef in Vaccination Queue/info.md
+++ codechef/1000-1200 - Beginner Level/9. Chef in Vaccination Queue/info.md
@@ -1,5 +1,5 @@
 Difficulty: 1016
 
-Attempts: 
+Attempts: 1
 
 Link: https://www.codechef.com/problems/VACCINQ
--- codechef/1000-1200 - Beginner Level/9. Chef in Vaccination Queue/main.cpp
+++ codechef/1000-1200 - Beginner Level/9. Chef in Vaccination Queue/main.cpp
@@ -5,7 +5,25 @@
 #include <vector>
 using namespace std;
 
-void solve() {}
+void solve() {
+    int n, p, x, y;
+    cin >> n >> p >> x >> y;
+    int arr[n];
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+    int time = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (i != p) {
+            if (arr[i - 1])
+                time += y;
+            else
+                time += x;
+        } else {
+            time += y;
+            break;
+        }
+    }
+    cout << time << "\n";
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
