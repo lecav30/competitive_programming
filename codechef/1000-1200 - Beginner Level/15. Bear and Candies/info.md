666c249283c738c94ddc22e641f5c1a2f7422b63
lecav30 <lecav30@outlook.com>
2023-01-22 18:29:15 -0500

    Unsolved

---
+++ codechef/1000-1200 - Beginner Level/15. Bear and Candies/info.md
@@ -0,0 +1,5 @@
+Difficulty: 1028
+
+Attempts: 1
+
+https://www.codechef.com/problems/CANDY123
---
+++ codechef/1000-1200 - Beginner Level/15. Bear and Candies/main.cpp
@@ -0,0 +1,22 @@
+#include <limits.h>
+
+#include <algorithm>
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve() {}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--) solve();
+    /* solve(); */
+    return 0;
+}
