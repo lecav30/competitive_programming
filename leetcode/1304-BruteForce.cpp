9024424ec383cfbfc71f03596180464eee7eab2e
lecav30 <lecav30@outlook.com>
2025-09-08 15:23:37 -0500

    refactor: improve 1304

--- leetcode/1304-BruteForce.cpp
+++ leetcode/1304-BruteForce.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> ans;
-        int i = n / 2 * -1;
-        while (ans.size() < n) {
-            if (i == 0 && n % 2 == 0) {
-                ++i;
-                continue;
-            } // even
-            ans.push_back(i);
-            ++i;
+        ans.reserve(n);
+        for (int k = 1; k <= n/2; ++k) {
+            ans.push_back(k);
+            ans.push_back(-k);
         }
+        if (n % 2 == 1) ans.push_back(0); // add zero on odd input
         return ans;
     }
 };
