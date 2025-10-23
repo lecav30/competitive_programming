724945a127533352c62fdd6aad0b5b92fb524090
Eduardo Sebastián Lévano Cavero <lecav30@outlook.com>
2025-10-23 14:19:32 -0500

    feat: just simulate until it works

---
+++ leetcode/3461-Simulation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        while (s.length() > 2) {
+            string temp = "";
+            for (int i = 0; i < s.length() - 1; ++i)
+                temp += ((s[i] - '0') + (s[i + 1]) - '0') % 10;
+            s = temp;
+            
+            if (s.length() == 2 && s[0] == s[1])
+                return true;
+        }
+        return false;
+    }
+};
